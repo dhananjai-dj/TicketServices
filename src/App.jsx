3adf4cc4365c99474bc9943cd50a48a008fecc14
dhananjai-dj <119773597+dhananjai-dj@users.noreply.github.com>
2024-07-19 11:28:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
 
       <Router>
         <Routes>
-          <Route path=".../" element={<Login />} />
+          <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
